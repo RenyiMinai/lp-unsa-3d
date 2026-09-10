ee79c289baf32fd11764c43a378074cdd9a16c71
RenyiMinai <migatitolindo888@gmail.com>
2026-09-09 23:47:58 -0500

    Versión inicial LP-UNSA 3D congreso

---
+++ catalogo.js
@@ -0,0 +1,8 @@
+export const catalogo = [
+{id:"PUC-001",codigo:"PU/C-001",identificacion:"Ammonoidea indet.",localidad:"Yura, Arequipa, Perú",unidad:"Por confirmar",edad:"Jurásico",modelo:"modelos/PUC-001.glb",descarga:true},
+{id:"PUC-002",codigo:"PU/C-002",identificacion:"Ammonoidea indet.",localidad:"Yura, Arequipa, Perú",unidad:"Por confirmar",edad:"Jurásico",modelo:"modelos/PUC-002.glb",descarga:true},
+{id:"PUC-005",codigo:"PU/C-005",identificacion:"Ammonoidea indet.",localidad:"Yura, Arequipa, Perú",unidad:"Por confirmar",edad:"Jurásico",modelo:"modelos/PUC-005.glb",descarga:true},
+{id:"PUC-013",codigo:"PU/C-013",identificacion:"Ammonoidea indet.",localidad:"Yura, Arequipa, Perú",unidad:"Por confirmar",edad:"Jurásico",modelo:"modelos/PUC-013.glb",descarga:true},
+{id:"PUC-017",codigo:"PU/C-017",identificacion:"Ammonoidea indet.",localidad:"Yura, Arequipa, Perú",unidad:"Por confirmar",edad:"Jurásico",modelo:"modelos/PUC-017.glb",descarga:true},
+{id:"PUC-197",codigo:"PU/C-197",identificacion:"Ammonoidea indet.",localidad:"Yura, Arequipa, Perú",unidad:"Por confirmar",edad:"Jurásico",modelo:"modelos/PUC-197.glb",descarga:true}
+];
